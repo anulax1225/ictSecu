22770d109f62965919f58f7b9b7fc9a071cb66f5
anulax1225 <anulax1225@icloud.com>
2025-02-20 14:46:18 +0100

    ff

--- app/routes/web.php
+++ app/routes/web.php
@@ -22,7 +22,7 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::post("/transfert", [BankController::class, 'transfert']);
+    Route::post("/transfert", [BankController::class, 'transfert'])->middleware("App\Http\Middleware\Cors");
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
